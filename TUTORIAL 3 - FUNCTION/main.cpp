f7523dfb47fae1e36311abe960e5f72bb1eabb35
Danialhalim680 <danialhalim680@gmail.com>
2024-10-29 16:14:51 +0800

    Initial Commit

---
+++ TUTORIAL 3 - FUNCTION/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TUTORIAL 3 - FUNCTION
+//
+//  Created by Danial Halim on 29/10/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
